d50d812efe92bd7502894862b1f7c5aa1da90e90
Brendan D N <112511880+BrendanDN@users.noreply.github.com>
2022-12-17 18:42:13 +0000

    pre-caching assets

--- sw.js
+++ sw.js
@@ -1,6 +1,28 @@
+const staticCache = 'site-static'
+const assets = [
+  '/',
+  '/index.html',
+  '/auth.html',
+  '/auth.html?\auth=signin',
+  '/auth.html?\auth=signup',
+  '/studio.html',
+  '/watch.html',
+  '/js/stores.js',
+  '/js/components.js',
+  '/css/style.css',
+  'https://cdn.jsdelivr.net/npm/gun/gun.min.js',
+  'https://cdn.jsdelivr.net/npm/gun/sea.js'
+];
+
 //install listner
 self.addEventListener('install', evt => {
   console.log("Service Worker Has Been Installed")
+  evt.waitUntil(
+    caches.open(staticCache).then(cache => {
+      console.log('Caching Shell Assets');
+      cache.addAll(assets);
+    })
+  )
 })
 
 //activate listner
